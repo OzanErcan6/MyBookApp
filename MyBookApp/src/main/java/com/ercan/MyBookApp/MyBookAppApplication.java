baad9845967be32b5da25f08c43023d275fa4bec
Ozan ERCAN <ozanercann@gmail.com>
2023-06-16 17:44:20 +0300

    project created

---
+++ MyBookApp/src/main/java/com/ercan/MyBookApp/MyBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.ercan.MyBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookAppApplication.class, args);
+	}
+
+}
---
+++ MyBookApp/src/test/java/com/ercan/MyBookApp/MyBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ercan.MyBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
