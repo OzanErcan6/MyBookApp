ed03176fd5dde5e32054b021d0aae6b18b1c78bf
OzanErcan6 <ozanercann@gmail.com>
2023-06-24 17:25:11 +0300

    insert books sql

---
+++ MyBookApp/src/main/resources/inserts.sql
@@ -0,0 +1,4 @@
+insert into book (id, isbn, title, writer) values (1, '123', 'aaaa', 'a');
+insert into book (id, isbn, title, writer) values (2, '1234', 'bbbb', 'b');
+insert into book (id, isbn, title, writer) values (3, '1235', 'cccc', 'c');
+insert into book (id, isbn, title, writer) values (4, '1236', 'dddd', 'd');
